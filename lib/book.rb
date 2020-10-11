d51fa4e03d881714f575bb0e29f4afe6cf420bc3
wickedvic <viksipart@gmail.com>
2020-10-11 19:32:31 -0400

    final to Object Orientation Basics

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,18 @@
+class Book
+attr_accessor :title, :author, :page_count, :genre
+    def initialize(title)
+      @title = title 
+      @author = author 
+      @page_count = page_count
+      @genre = genre  
+    end
 
+def turn_page
+puts "Flipping the page...wow, you read fast!"
+
+end
+
+
+
+end 
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,35 @@
 # Make your shoe class here!
+
+class Shoe
+    attr_accessor :brand, :color, :size, :material, :condition 
+    
+
+    def initialize(brand)
+
+        @brand = brand
+        @color = color
+        @size = size
+        @material = material 
+        @condition = condition  
+
+
+
+    end
+
+
+    def cobble
+      puts "Your shoe is as good as new!"
+@condition = "new"
+    end
+
+
+
+
+
+
+
+
+
+
+
+end 
